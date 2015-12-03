969a4151adcacd39367c07ba952b82ad9cda6b97
Alex Massaad <alex.massaad@shopify.com>
2015-12-03 18:54:23 -0500

    Added basic camera image v1

--- app/workers/camera_worker.rb
+++ app/workers/camera_worker.rb
@@ -2,6 +2,15 @@ class CameraWorker
   include Sidekiq::Worker
 
   def perform
-    #just DO it
+    @http_conn = Faraday.new do |builder|
+      builder.adapter Faraday.default_adapter
+    end
+    @u = "alex"
+    @p = "testing"
+
+    @http_conn.basic_auth(@u, @p)  unless @u.nil? && @p.nil?
+    response = @http_conn.get 'http://webcam.zzv.ca:1394/snapshot.cgi'
+
+    File.open('public/york.jpg', 'wb') { |fp| fp.write(response.body) }
   end
 end
