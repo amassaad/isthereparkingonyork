b04392705abd59e87d629da1e339f438740ac410
Alex Massaad <alex.massaad@shopify.com>
2015-12-03 19:26:02 -0500

    quote things

--- app/workers/camera_worker.rb
+++ app/workers/camera_worker.rb
@@ -13,6 +13,6 @@ class CameraWorker
     @http_conn.basic_auth(@u, @p)  unless @u.nil? && @p.nil?
     response = @http_conn.get 'http://webcam.zzv.ca:1394/snapshot.cgi'
 
-    File.open('public/york.jpg', 'wb') { |fp| fp.write(response.body) }
+    File.open("public/york.jpg", "wb") { |fp| fp.write(response.body) }
   end
 end
