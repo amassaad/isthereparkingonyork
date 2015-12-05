af4bc24aed4f2b5f07978e89c53b54a03e9090ff
Alex Massaad <alex.massaad@shopify.com>
2015-12-05 09:55:57 -0500

    carrier wave and s3

--- app/workers/camera_worker.rb
+++ app/workers/camera_worker.rb
@@ -2,8 +2,7 @@ class CameraWorker
   include Sidekiq::Worker
 
   def perform
-    logger.info "Things are happening."
-    logger.debug "Here's some info: #{hash.inspect}"
+    logger.info "Things are happening in the cameara loader."
     #todo with s3 support
     @http_conn = Faraday.new do |builder|
       builder.adapter Faraday.default_adapter
@@ -13,7 +12,11 @@ class CameraWorker
 
     @http_conn.basic_auth(@u, @p)  unless @u.nil? && @p.nil?
     response = @http_conn.get 'http://webcam.zzv.ca:1394/snapshot.cgi'
+    logger.info "Trying to make save happen. With success?"
 
-    File.open("public/york.jpg", "wb") { |fp| fp.write(response.body) }
+    WebcamUploader.new.store!(response.body)
+
+    logger.info "Saved file. With success?"
+    # File.open("public/york.jpg", "wb") { |fp| fp.write(response.body) }
   end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,10 @@
-# CarrierWave.configure do |config|
-#   config.fog_provider = 'fog/aws'                        # required
-#   config.fog_credentials = {
-#     provider:              'AWS',                        # required
-#     aws_access_key_id:     'xxx',                        # required
-#     aws_secret_access_key: 'yyy',                        # required
-#   }
-#   config.fog_directory  = 'name_of_directory'                          # required
-# end
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV['PARKING_KEY'],                        # required
+    aws_secret_access_key: ENV['PARKING_ID'],
+    region:                'us-west-2'                           # required
+  }
+  config.fog_directory  = 'yorkparking'                          # required
+end
