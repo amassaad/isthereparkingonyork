73813c7ac5902f9489c1767c671db8a0079e98ae
amassaad <massaad@gmail.com>
2016-09-28 18:40:37 -0400

    Try with cloudfront assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
-
+  config.action_controller.asset_host = "deb26b43653sm.cloudfront.net"
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
