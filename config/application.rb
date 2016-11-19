7c8d04a26217e53e6eb5fa57b1772b050e190514
Alex Massaad <alex.massaad@shopify.com>
2016-11-18 19:43:33 -0500

    rails five

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require "active_model/railtie"
 require "action_controller/railtie"
@@ -6,12 +6,14 @@ require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Isthereparkingonyork
   class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
