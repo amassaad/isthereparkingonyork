fb1e6f757717c08c999145c303aa78bcc2341b83
Alex Massaad <alex.massaad@shopify.com>
2017-03-03 18:04:58 -0500

    Add settings code back to worker

--- test/controllers/parking_controller_test.rb
+++ test/controllers/parking_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ParkingControllerTest < ActionController::TestCase
-  test "should get check" do
+  test "should get check and should queue one job" do
     get :check
     assert_response :success
   end
