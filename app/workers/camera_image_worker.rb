094f9aacf984b37e425fafdde8f1052d4244116b
Alex Massaad <alex.massaad@shopify.com>
2017-05-16 09:50:08 -0400

    Add c2 destination

--- app/workers/camera_image_worker.rb
+++ app/workers/camera_image_worker.rb
@@ -7,7 +7,7 @@ class CameraImageWorker
   def perform(id)
     @u = ENV['SECURE_USER']
     @p = ENV['SECURE_PASS']
-    
+
     StatsD.measure('CameraImageWorker.performance') do
       logger.info "Things are starting to happen in the camera Image worker job."
       StatsD.increment('CameraImageWorker.start')
@@ -16,8 +16,15 @@ class CameraImageWorker
         builder.adapter Faraday.default_adapter
       end
 
-      @http_conn.basic_auth(@u, @p)
-      response = @http_conn.get "http://site.zzv.ca:1394/snapshot.cgi"
+      # @http_conn.basic_auth(@u, @p)
+
+      # response = @http_conn.get "http://site.zzv.ca:1394/snapshot.cgi"
+      #
+      #
+      #
+      # http://ip address:port/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=admin&pwd=xxx
+
+      response = @http_conn.get "http://site.zzv.ca:1394/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=#{@u}&pwd=#{@p}"
       logger.info "Trying to make save happen. With success? [status code: #{response.status}]"
 
       File.open("tmp/york.jpg", "wb") { |fp| fp.write(response.body) }
